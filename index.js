2bc1fcb0fc90b2c13cda63e9f00528b82cc4820e
Tin Wong <9789311+tin-wong@users.noreply.github.com>
2022-08-20 14:47:19 -0700

    Add deleteMany to delete all documents

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ async function main() {
   await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 }
 
-
 // Create an Exercise model
 const exerciseSchema = new mongoose.Schema({
     username: String,
@@ -26,27 +25,53 @@ const exerciseSchema = new mongoose.Schema({
     userId: String
 })
 
-const exercise = mongoose.model('exercise', exerciseSchema);
+const Exercise = mongoose.model('exercise', exerciseSchema);
 
 // Create an user model
 const userSchema = new mongoose.Schema({
   username: String
 })
 
-const user = mongoose.model('user', userSchema);
+const User = mongoose.model('user', userSchema);
 
-// Create a log model
-const logSchema = new mongoose.Schema({
-  username: String,
-  count :Number,
-  log: [{
-    description: String,
-    duation: Number,
-    date: Date
-  }]
-})
+// // Create a log model
+// const logSchema = new mongoose.Schema({
+//   username: String,
+//   count :Number,
+//   log: [{
+//     description: String,
+//     duation: Number,
+//     date: Date
+//   }]
+// })
 
-const log = mongoose.model('log', logSchema);
+// const log = mongoose.model('log', logSchema);
+
+// Delete all exisiting documents in the database. Then initialize it with a document. 
+User.deleteMany({}, (error, mongooseDeleteResult) => {
+  if(error) return console.error(error);
+  console.log(mongooseDeleteResult);
+  let newUser = new User({username: 'Test User 1'});
+  newUser.save((err) => {
+      if(err) return console.error(err);
+  })
+});
+
+Exercise.deleteMany({}, (error, mongooseDeleteResult) => {
+  if(error) return console.error(error);
+  console.log(mongooseDeleteResult);
+  let newUser = User.find({username: 'Test User 1'})
+  let newExercise = new Exercise({
+    username: newUser.username,
+    description: "Test Exercise 1",
+    duration: 10,
+    date: new Date().toDateString(),
+    userId: newUser._id
+  });
+  newExercise.save((err) => {
+      if(err) return console.error(err);
+  })
+});
 
 app.use(cors())
 app.use(express.static('public'))
@@ -55,14 +80,14 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/users', async (req, res, next) => {
-  const newUser = new user({username: req.body.username});
+  const newUser = new User({username: req.body.username});
   newUser.save((err, result) => {
     if (err) return err;
     res.json({username: result.username, _id: result.id})
     next();
   });
 }).get('/api/users', async (req, res, next) => {
-  const allUsers = await user.find();
+  const allUsers = await User.find();
   res.json(allUsers);
   next();
 })
@@ -73,8 +98,8 @@ app.post('/api/users/:_id/exercises', async (req, res, next) => {
   } else {
     newDate = new Date(req.body.date)
   }
-  const findUser = await user.findById(req.params._id);
-  const newExercise = new exercise({
+  const findUser = await User.findById(req.params._id);
+  const newExercise = new Exercise({
     userId: findUser._id, 
     username: findUser.username, 
     date: newDate.toDateString(),
@@ -95,8 +120,8 @@ app.post('/api/users/:_id/exercises', async (req, res, next) => {
 });
 
 app.get('/api/users/:_id/logs', async (req, res, next) => {
-  const exerciseCount = await exercise.count({userId: req.params._id});
-  const exerciseList = await exercise.find({userId: req.params._id});
+  const exerciseCount = await Exercise.count({userId: req.params._id});
+  const exerciseList = await Exercise.find({userId: req.params._id});
   //const getUser = await user.find({_id: req.params._id});
   
   let log = [];
